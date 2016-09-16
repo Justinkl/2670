82a5e28fa38b853a2ba9810b9c47b9e646157ec4
Unknown <justinleroyklosinski@gmail.com>
2016-09-15 22:32:15 -0700

    Variable understanding

---
+++ variable understanding/Assets/Variable understanding.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Variableunderstanding : MonoBehaviour {
+
+    public float gravity = 5.0f;
+    public int maximumOverdrive = 100;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ variable understanding/Assets/variables.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class variables : MonoBehaviour {
+
+    public float gravity = 5;
+    public int maximumOverdrive = 100;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
