f38743d65f302c7fb5ebd68e6189ac67b129fe61
Unknown <justinleroyklosinski@gmail.com>
2016-09-17 22:37:18 -0700

    ConventionsAndSyntax

---
+++ Conventions and syntax/Assets/ConventionsAndSyntax.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConventionsAndSyntax : MonoBehaviour {
+    //A";" is going to terminate the end of this variable
+    public float speed = 100;
+
+
+	// Use this for initialization
+	void Start () {
+        // indenting helps organize code
+	if(speed > 99)
+        {
+            print("GOTTA GO FAST");
+        }
+    // I will disable the bottom piece by making it muti-line comment like the example in te videos
+    /*
+    if(speed < 101)
+        {
+            print("YOU'RE TOO SLOW!");
+        }
+    */
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
