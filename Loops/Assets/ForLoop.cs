3a5fa21504bbbf7b5d8a6b7f333f28c928cf15b6
Unknown <justinleroyklosinski@gmail.com>
2016-09-20 22:46:11 -0700

    Loops(For&While)

---
+++ Loops/Assets/ForLoop.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ForLoop : MonoBehaviour {
+
+    private int WallDistance = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        WallWalking();
+	
+	}
+    void WallWalking()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            WallDistance = Random.Range(1, 20);
+
+            if(WallDistance >= 15)
+            {
+                print("Is that wall over there?");
+
+            }
+            if (WallDistance >= 5 && WallDistance <= 10)
+            {
+                print("I'm getting to close!");
+            }
+            if (WallDistance >= 1 && WallDistance <=4)
+            {
+                print("Ah Crap!");
+            }
+            if (WallDistance <= 0)
+            {
+                print("SPLAT!");
+            }
+
+        }
+    }
+
+}
---
+++ Loops/Assets/WhileLoop.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WhileLoop : MonoBehaviour
+{
+    //Remove ForLoop component and attach this one to the Empty GameObject
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        WallWalking();
+
+    }
+    void WallWalking()
+    {
+        int i = 0;
+
+        while (i != 9)
+        {
+            i++;
+            print(i);
+        }
+    }
+}
