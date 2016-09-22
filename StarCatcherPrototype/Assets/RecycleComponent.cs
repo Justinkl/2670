5dfd71fbafda617675faccc71e5c273bff8986d1
justinkl <justinleroyklosinski@gmail.com>
2016-09-22 13:16:06 -0600

    Updated StarCatcher&script

---
+++ StarCatcherPrototype/Assets/RecycleComponent.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class RecycleComponent : MonoBehaviour 
+{
+
+	private Vector3 newLocation;
+	/*private int i = 0;
+	public List<Recycler> recycleAbleList;
+
+	void Start()
+	{
+		recycleAbleList = new List<Recycler>();
+		Recycler.RecycleAction += RecycleActionHandler;
+	}
+
+	private void RecycleActionHandler(Recycler obj)
+	{
+		recycleAbleList.Add(obj);
+	}
+
+*/
+	void OnTriggerEnter ()
+	{
+		newLocation.x = StaticVar.nextSectionPos;
+		transform.localPosition = newLocation; //---->recycleAbleList[i].cube.position = newLocation;
+		StaticVar.nextSectionPos += StaticVar.distance;
+
+		//if(i == recycleAbleList.Count-1)
+		//{i++;
+		//}
+	}
+}
---
+++ StarCatcherPrototype/Assets/StaticVar.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StaticVar 
+
+{
+	public static float distance = 54.7f;
+	public static float nextSectionPos = 273.5f;
+}
