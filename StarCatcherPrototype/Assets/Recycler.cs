d18d52d4318b110f155f5e7de4915639ffba883c
Unknown <justinleroyklosinski@gmail.com>
2016-09-21 19:57:39 -0600

    StarCatcherPrototype

---
+++ StarCatcherPrototype/Assets/Recycler.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Recycler : MonoBehaviour {
+
+    public static Action<Recycler> RecycleAction;
+    public bool canBeRecycled;
+    public Transform cube;
+
+    void Start()
+    {
+        gameObject = this.GetComponent<Transform>();
+        if (RecycleAction != null && canBeRecycled)
+        {
+            RecycleAction(this);
+        }
+    }
+
+    void OnTriggerEnter()
+    {
+        canBeRecycled = true;
+        Start();
+    }
+
+}
