0292a71b0581c0122c3235c8f700cae34eb00e47
Unknown <justinleroyklosinski@gmail.com>
2016-10-03 13:57:07 -0600

    flipcharacter

--- StarCatcherPrototype/Assets/StaticVar.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class StaticVar 
-
-{
-	public static float distance = -0.4f;
-	public static float nextSectionPos = -1.3f;
-}
--- StarCatcherProtoype0.3/Assets/Statics.cs
+++
@@ -1,7 +0,0 @@
-﻿public class Statics
-{
-
-    public static float distance = 4;
-    public static float nextPostion = 4;
-
-}
