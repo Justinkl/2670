284f474f34d49993c04b26a9795902636ed3be89
Unknown <justinleroyklosinski@gmail.com>
2016-10-14 13:47:50 -0600

    adding stuff but primarily the delegates

---
+++ prject1/Assets/UserInput.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class UserInput : MonoBehaviour {
+
+    public static Action<KeyCode> UserInput;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow) && UserInput != null)
+        {
+            UserInput(KeyCode.UpArrow);
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow) && UserInput != null)
+        {
+            UserInput(KeyCode.DownArrow);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && UserInput != null)
+        {
+            UserInput(KeyCode.LeftArrow);
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow) && UserInput != null)
+        {
+            UserInput(KeyCode.RightArrow);
+        }
+    }
+}
