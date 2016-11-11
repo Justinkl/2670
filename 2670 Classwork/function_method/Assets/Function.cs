9b56e551f188070c8e585bd530e6bc9b835b6110
Unknown <justinleroyklosinski@gmail.com>
2016-11-11 14:34:18 -0800

    Added late functions for Anthony

---
+++ 2670 Classwork/function_method/Assets/Function.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Function : MonoBehaviour {
+
+    int myInt = 3001;
+
+    //Use this for initialization
+    void Start()
+    {
+        myInt = MultiplyByThree(myInt);
+        Debug.Log(myInt);
+    }
+
+
+    int MultiplyByThree(int number)
+    {
+        int ret;
+        ret = number * 3;
+        return ret;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (myInt > 9000)
+        {
+            print("IT'S OVER NINE THOUSAAAAAND!!");
+        }
+
+	
+	}
+}
