483e1aa5c7a3a06273826d2daa78e87a62baf6e2
Justin Klosinski <justinleroyklosinski@gmail.com>
2016-11-12 13:14:05 -0700

    Fixing game and AItesting

---
+++ StarCatcherProtoype0.3/Assets/GraphicsFix.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GraphicsFix : MonoBehaviour {
+
+	public Transform rotateRef;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.rotation.y = rotateRef.transform.rotation.y;
+	}
+}
