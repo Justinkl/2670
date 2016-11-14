d5818d27da811291ca40dbc39967371149101fa8
Unknown <justinleroyklosinski@gmail.com>
2016-11-14 14:02:10 -0700

    Interface lesson

---
+++ 2670 Classwork/Power/Assets/HealthPowerUp.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthPowerUp : MonoBehaviour, IPowerUp {
+
+    public int power { get;  }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void OnTriggerEnter () {
+	
+	}
+}
