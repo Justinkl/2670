da6f60b4cd3bf4a2f813c17459a372ca9a86b9e9
Unknown <justinleroyklosinski@gmail.com>
2016-11-30 13:01:29 -0800

    Delegates and events

---
+++ 2670 Classwork/aeeay/Assets/DelegateIntro.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System;
+
+public class DelegateIntro : MonoBehaviour {
+
+    public static Action MyEvent;
+    public static Action<string> EventWithArgs;
+
+    public static Action<string> EventString;
+
+    public delegate void DelWithArgs(string s);
+    public static DelWithArgs EventReturnArgs;
+
+    public delegate string DelString();
+
+	// Use this for initialization
+	void Start () {
+
+        string data = EventReturnArgs("Cat");
+
+        print(EventString());
+
+        EventWithArgs("Hello World");
+
+        if (MyEvent != null)
+            MyEvent();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- 2670 Classwork/aeeay/Assets/EventHandlerIntro.cs
+++ 2670 Classwork/aeeay/Assets/EventHandlerIntro.cs
@@ -1,15 +1,42 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class EventHandlerIntro : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+        DelegateIntro.MyEvent += MyEventHandler;
+        DelegateIntro.EventWithArgs += EventWithArgsHandler;
+        DelegateIntro.EventString += EventStringHandler;
+        DelegateIntro.EventReturnArgs += EventReturnArgsHandler;
+
 	
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    private string EventReturnArgsHandler(string s)
+    {
+        return
+    }
+
+    private string EventStringHandler()
+    {
+        print("Do you like cats?");
+        return ("I hate cats");
+    }
+
+    private void MyEventHandler()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void EventWithArgsHandler(string s)
+    {
+        throw new NotImplementedException();
+    }
+
+    // Update is called once per frame
+    void Update () {
 	
 	}
 }
