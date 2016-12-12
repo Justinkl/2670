e6f0305ede634a09ff1c3ae107bbb51c0427d402
Unknown <justinleroyklosinski@gmail.com>
2016-12-11 16:59:57 -0800

    practice scripts

---
+++ 2670 Classwork/ZooGame/Assets/TransitionCoroutine.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class TransitionCoroutine : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine("Countdown");
+	
+	}
+	
+	// Update is called once per frame
+	private IEnumerator Countdown ()
+    {
+        yield return new WaitForSeconds(10);
+        SceneManager.LoadScene(0);
+	}
+}
---
+++ 2670 Classwork/ZooGame/Assets/WelcomeSwitchStatement.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WelcomeSwitchStatement : MonoBehaviour {
+
+    public int welcome = 3;
+    //private bool wellhellotherevar = true;
+	// Use this for initialization
+	void Start () {
+
+        Intro();
+    }
+	
+	public void Intro () {
+        switch (welcome)
+        {
+            case 3:
+                //StartCoroutine("wellhellothere");
+                print("Well hello there!");
+                goto case 2;
+            case 2:
+                print("Welcome to Justin's collection!");
+                goto case 1;
+            case 1:
+                print("We have a special treat for you today so let's not waste time!");
+                goto default;
+            default:
+                print("Just follow me and have fun!");
+                break;
+          }
+        }
+	
+       //private IEnumerator Wellhellothere ()
+         //  {
+        //if(wellhellotherevar == true){
+          //  wellhellotherevar = false;
+        //}
+         //   yield return new WaitForSeconds(3);
+       // wellhellotherevar = true;
+        
+	//}
+}
