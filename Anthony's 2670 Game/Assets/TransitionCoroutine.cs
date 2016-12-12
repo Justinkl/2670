dbfc82fefead9ed6836e7ac967dbc9f121d37515
Justin Klosinski <justinleroyklosinski@gmail.com>
2016-12-11 18:01:00 -0700

    taking practice scripts over

---
+++ Anthony's 2670 Game/Assets/TransitionCoroutine.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class TransitionCoroutine : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine ("Countdown");
+	}
+
+	private IEnumerator Coundown ()
+	{
+		yield return new WaitForSeconds (10);
+		SceneManager.LoadScene (0);
+	}
+}
--- Anthony's 2670 Game/Assets/WelcomeSwitchStatement.cs
+++ Anthony's 2670 Game/Assets/WelcomeSwitchStatement.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CoroutineOne : MonoBehaviour {
+public class WelcomeSwitchStatement : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
